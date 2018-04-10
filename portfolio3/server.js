37dedf2310bfac0750996b127df8036a89eb8059
Gerardo Fernandez <gerardojose13@gmail.com>
2018-04-09 20:19:09 -0700

    adds mongoose connection

--- portfolio3/server.js
+++ portfolio3/server.js
@@ -5,7 +5,16 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-mongoose.connect("mongodb://localhost/gerardoProfile");
+var MONGODB_URI =
+  process.env.MONGODB_URI ||
+  "mongodb://heroku_d1swb4n2:vcckbuvaop1slgnsd0tjrhgfpm@ds239359.mlab.com:39359/heroku_d1swb4n2";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
+
+// mongoose.connect("mongodb://localhost/gerardoProfile");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
